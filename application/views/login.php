b002cd94c20939ea7936d9d01980738a97b937fd
danigo252 <danielgonzalez252@gmail.com>
2016-11-10 21:06:21 -0400

    Implementacion de UI

---
+++ application/views/login.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Ingresar al sistema </title>
+    <link rel="stylesheet" type="text/css" href="<?=base_url();?>static/css/vendor/bootstrap/css/bootstrap.min.css"/>
+    <link rel="stylesheet" type="text/css" href="<?=base_url();?>static/css/flat-ui.min.css"/>
+</head>
+<body>
+<div class="container">
+
+    <div class="row">
+        <div class="col-md-4  col-md-offset-4">
+            <h2>Login</h2
+            <form action=" "  method="POST">
+
+                <div class="form-group">
+                    <label for="username"></label>
+                    <input type="text" name="username" id="username" class="form-control" placeholder="Username">
+                </div>
+
+                <div class="form-group">
+                    <label for="password"></label>
+                    <input type="password" name="password" id="password" class="form-control" placeholder="Password">
+                </div>
+
+                <input type="submit" value="Ingresar" class="btn btn-primary">
+            </form>
+        </div>
+    </div>
+</div>
+</body>
+</html>
