39991803712d7a0386b63f3148a0e269dd7ca422
gianelo <gianbarboza11@gmail.com>
2016-11-21 19:58:40 -0400

    build users grud

--- application/views/body/user/user_main.php
+++ application/views/body/user/user_main.php
@@ -1,7 +1,7 @@
 <div class="container">
 	<div class="row">
 		<ul class="nav nav-tabs">
-		<li role="presentation" class="active"><a href="<?=base_url();?>users/index">Users Register</a></li>
+		<li role="presentation" class="active"><a href="<?=base_url();?>users">Users Register</a></li>
 			<li role="presentation"><a href="<?=base_url();?>users/add_user">Add By Administrator</a></li>
 			<li role="presentation"><a href="#">Messages</a></li>
 		</ul>
